ffb978e5f63c25e852a5c20cd3cee1cecb1c77bb
Byung-Jun <53166205+Byung-Jun@users.noreply.github.com>
2020-09-30 21:39:35 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -5,11 +5,6 @@ var app = http.createServer(function(request,response){
     if(request.url == '/'){
       url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
-      response.writeHead(404);
-      response.end();
-      return;
-    }
     response.writeHead(200);
     response.end(fs.readFileSync(__dirname + url));
  
